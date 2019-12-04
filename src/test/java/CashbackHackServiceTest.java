825fd79585a6eb884c35d255ac9028744c317889
denis290788 <denis290788@gmail.com>
2019-12-04 21:01:48 +0300

    commit 2

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -14,4 +14,15 @@ class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shouldReturn100IfAmount900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
 }
