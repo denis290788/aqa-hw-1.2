578fbc56cf1dbe2b34829198850db37edb7627e4
denis290788 <denis290788@gmail.com>
2019-12-04 22:04:59 +0300

    commit 2

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,27 +1,16 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-    @Test
-    void shouldReturn0IfAmount1000() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackData.csv", numLinesToSkip = 1)
+    void shouldReturnCashback(int amount, int expected) {
         CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-
         int actual = cashbackHackService.remain(amount);
-        int expected = 0;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldReturn100IfAmount900() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
-
-        int actual = cashbackHackService.remain(amount);
-        int expected = 100;
 
         assertEquals(expected, actual);
     }
